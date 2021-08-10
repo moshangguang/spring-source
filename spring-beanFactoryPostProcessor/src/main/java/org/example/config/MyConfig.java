d77f9cdbfe25d105cfe643d877b6472f042d2a65
moshangguang <3208945316@qq.com>
2021-08-10 23:13:56 +0800

    开发spring源码解析

--- spring-beanFactoryPostProcessor/src/main/java/org/example/config/MyConfig.java
+++ spring-beanFactoryPostProcessor/src/main/java/org/example/config/MyConfig.java
@@ -1,20 +1,8 @@
 package org.example.config;
 
-import org.example.pojo.C;
-import org.example.registrar.MyConfig4Registrar;
 import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan("org.example.service")
-@Import(MyConfig4Registrar.class)
 public class MyConfig {
-
-    @Bean
-    public C c() {
-        return new C();
-    }
-
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(MyConfig.class);
-    }
 }
